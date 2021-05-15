436d548af723d97ec5dcc2099dbcb58c3de388d5
Nikita7181 <nik.mishin.88@mail.ru>
2021-05-15 23:33:25 +0300

    new style

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <string>
 
-using namespase std;
-
 int main()
 {
+<<<<<<<< HEAD
+	std::string name;// user name
+	std::gitline(std::cin, name);// read user name
+=======
 	std::string name;
-	std::gitline(std::cin, name);
+	std::getline(std::cin, name); // read user name
+>>>>>>>>b2013b3dela7544a82d92748c564a95ad8db3b93
 	cout << "Hello world" << name << std::endl;;
 	return 0;
 }
